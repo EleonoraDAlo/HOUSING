b3a2227262dfa82925ca25282e9254310168af31
Eleonora D'Alò <eleonoradalo@outlook.it>
2022-03-31 15:25:15 +0200

    Commit1

---
+++ src/com/generation/classe/room/Room.java
@@ -0,0 +1,18 @@
+package com.generation.classe.room;
+
+public class Room
+ {
+
+    public int s1, s2;
+
+    public Room(int s1, int s2)
+    {
+        this.s1=s1;
+        this.s2=s2;
+    }
+
+
+
+
+
+}
---
+++ src/com/generation/housing/MainHouse.java
@@ -0,0 +1,19 @@
+package com.generation.housing;
+
+import com.generation.classe.room.Room;
+
+public class MainHouse 
+{
+
+    public static void main(String[] args) {
+        
+
+        Room r1 = new Room(5,6);
+        int smq = 800;
+
+        System.out.println("Total price");
+
+
+
+    }
+}
